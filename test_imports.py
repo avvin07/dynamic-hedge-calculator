cb8b2e091915b885cece1a0b2e0771ed9aa2c7bf
Your Name <user@example.com>
2025-04-17 23:26:46 +0500

    Обновление файлов

--- test_imports.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-print("Импорт библиотек успешно выполнен") 
